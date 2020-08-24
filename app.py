eabb267346a3fed0b94a9e68afc8719ef72b14b0
Anna <missaol@icould.com>
2020-08-24 15:01:30 +0100

    Complete core exercises for Module 1

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import session_items as session
 
 app = Flask(__name__)
 app.config.from_object('flask_config.Config')
-
+print ("server starting")
 @app.route('/')
 def index():
-    return 'Hello World!'
+    print ("handling request")
+    Todos = session.get_items()
+    return render_template('index.html',Todos = Todos, name = 'Annas')
+
+@app.route('/add_item', methods=["POST"])
+def add_item():
+    session.add_item(request.form.get('title'))
+    print ("adding item")
+    flash("Item successfully added")
+    return redirect (url_for('index'))
 
 if __name__ == '__main__':
     app.run()
